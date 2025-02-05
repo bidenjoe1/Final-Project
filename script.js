24fd4e92edd7987ee5c34da2e70aa18f36cd2232
bidenjoe1 <nathan.k.waititu@gmail.com>
2025-02-05 20:12:05 +0300

    Create function and variables

---
+++ script.js
@@ -0,0 +1,8 @@
+function addBook(event) {
+    event.preventDefault();
+
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let summary = document.getElementById("summary").value;
+    let isRead = document.getElementById("isRead").checked;
+}
