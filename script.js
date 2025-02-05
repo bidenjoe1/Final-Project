544aaf1b835c87dd7d1060f4ddd3cbf9d2c5b3ab
bidenjoe1 <nathan.k.waititu@gmail.com>
2025-02-05 20:16:56 +0300

    Ensure no empty field

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ function addBook(event) {
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let summary = document.getElementById("summary").value;
-    let isRead = document.getElementById("isRead").checked;
+    let confirmRead = document.getElementById("isRead").checked;
+
+    if (title === "" || author === "") {
+        alert("Please fill in all required fields.");
+        return;
+    }
 }
