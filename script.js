904c71b3857feb98b546df5503e11fda3728a0c3
bidenjoe1 <nathan.k.waititu@gmail.com>
2025-02-05 22:31:27 +0300

    Changed summary to text box

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function addBook(event) {
 
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
-    let summary = document.getElementById("AboutBook").value;
+    let summary = document.getElementById("summary").value;
     let isRead = document.getElementById("isRead").checked;
 
     if (title === "" || author === "" || summary === "") {
